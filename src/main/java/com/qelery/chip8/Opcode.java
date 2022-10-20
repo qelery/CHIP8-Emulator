b76d264c3ba1d3daa3a2663afcce6a6a27cc4fa5
Quincy Elery <quincyelery@gmail.com>
2022-10-19 21:16:20 -0500

    Add Opcode class

---
+++ src/main/java/com/qelery/chip8/Opcode.java
@@ -0,0 +1,136 @@
+package com.qelery.chip8;
+
+
+import java.util.Objects;
+
+/**
+ * A class that represents an unsigned 2-byte opcode.
+ * <p>
+ * <h2>CPU opcode notation</h2>
+ * Adapted from: <a href="http://devernay.free.fr/hacks/chip8/C8TECH10.HTM">Cowgod's Chip-8 Technical Reference</a>
+ * <p>
+ * ■ ■ ■ ■ - 4-nibble representation of the 2-byte opcode
+ * <p>
+ * o &#9; 1st nibble of opcode &#9; ( o ■ ■ ■ )<br>
+ * x &#9; 2nd nibble of opcode &#9; ( ■ x ■ ■ )<br>
+ * y &#9; 3rd nibble of opcode &#9; ( ■ ■ y ■ )<br>
+ * n &#9; 4th nibble of opcode &#9; ( ■ ■ ■ n )<br>
+ * kk &#9; last 2 nibbles &#9; ( ■ ■ k k )<br>
+ * nnn &#9; last 3 nibbles &#9; ( ■ n n n )<br>
+ */
+public class Opcode {
+    private final int[] opcode;
+    private final int instruction;
+
+    public Opcode(int firstByte, int secondByte) {
+        this(((firstByte << 8) & 0xFF00) | (secondByte & 0x00FF));
+    }
+
+    public Opcode(int instruction) {
+        this.instruction = instruction;
+        this.opcode = new int[4];
+        this.opcode[0] = instruction >> 12 & 0x00F;
+        this.opcode[1] = instruction >> 8 & 0x00F;
+        this.opcode[2] = instruction >> 4 & 0x00F;
+        this.opcode[3] = instruction & 0x00F;
+    }
+
+    /**
+     * Returns the i-th nibble of the opcode
+     *
+     * @param index index of the nibble to return.
+     * @return the i-th nibble of the opcode
+     */
+    public int getNibble(int index) {
+        Objects.checkIndex(index, opcode.length);
+        return opcode[index];
+    }
+
+    /**
+     * Returns the first nibble of the opcode.
+     *
+     * @return the first nibble of the opcode
+     */
+    public int o() {
+        return getNibble(0);
+    }
+
+    /**
+     * Returns the second nibble of the opcode.
+     * <p>
+     * The notation 'x' comes from the CHIP-8's technical reference.
+     *
+     * @return the second nibble of the opcode
+     */
+    public int x() {
+        return getNibble(1);
+    }
+
+    /**
+     * Returns the third nibble of the opcode.
+     * <p>
+     * The notation 'y' comes from the CHIP-8's technical reference.
+     *
+     * @return the third nibble of the opcode
+     */
+    public int y() {
+        return getNibble(2);
+    }
+
+    /**
+     * Returns the fourth nibble of the opcode.
+     * <p>
+     * The notation 'n' comes from the CHIP-8's technical reference.
+     *
+     * @return the fourth nibble of the opcode
+     */
+    public int n() {
+        return getNibble(3);
+    }
+
+    /**
+     * Returns the last two nibbles of the opcode.
+     * <p>
+     * The notation 'kk' comes from the CHIP-8's technical reference.
+     *
+     * @return the last two nibbles of the opcode
+     */
+    public int kk() {
+        return instruction & 0xFF;
+    }
+
+    /**
+     * Returns the last three nibbles of the opcode.
+     * <p>
+     * The notation 'nnn' comes from the CHIP-8's technical reference.
+     *
+     * @return the last three nibbles of the opcode
+     */
+    public int nnn() {
+        return instruction & 0xFFF;
+    }
+
+    /**
+     * Returns the integer equivalent of the opcode.
+     *
+     * @return the integer equivalent of the opcode
+     */
+    public int intValue() {
+        return instruction;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Opcode opcode = (Opcode) o;
+
+        return instruction == opcode.instruction;
+    }
+
+    @Override
+    public int hashCode() {
+        return instruction;
+    }
+}
---
+++ src/test/java/com/qelery/chip8/OpcodeTest.java
@@ -0,0 +1,119 @@
+package com.qelery.chip8;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OpcodeTest {
+
+    Opcode opcode;
+    int instruction;
+    int nibble1, nibble2, nibble3, nibble4;
+
+    @BeforeEach
+    void setup() {
+        this.nibble1 = 0x1;
+        this.nibble2 = 0x2;
+        this.nibble3 = 0x3;
+        this.nibble4 = 0x4;
+
+        this.instruction = (nibble1 << 12) + (nibble2 << 8) + (nibble3 << 4) + nibble4;
+        this.opcode = new Opcode(instruction);
+    }
+
+    @Test
+    @DisplayName("Should get the retrieve the i-th nibble of the opcode")
+    void getNibble() {
+        assertEquals(nibble1, opcode.getNibble(0));
+        assertEquals(nibble2, opcode.getNibble(1));
+        assertEquals(nibble3, opcode.getNibble(2));
+        assertEquals(nibble4, opcode.getNibble(3));
+    }
+
+    @Test
+    @DisplayName("Should return the first nibble of the opcode")
+    void o() {
+        assertEquals(nibble1, opcode.o());
+    }
+
+    @Test
+    @DisplayName("Should return the second nibble of the opcode")
+    void x() {
+        assertEquals(nibble2, opcode.x());
+    }
+
+    @Test
+    @DisplayName("Should return the third nibble of the opcode")
+    void y() {
+        assertEquals(nibble3, opcode.y());
+    }
+
+    @Test
+    @DisplayName("Should return the fourth nibble of the opcode")
+    void n() {
+        assertEquals(nibble4, opcode.n());
+    }
+
+    @Test
+    @DisplayName("Should return the last two nibbles of the opcode")
+    void kk() {
+        int expectedKK = (nibble3 << 4) + nibble4;
+
+        assertEquals(expectedKK, opcode.kk());
+    }
+
+    @Test
+    @DisplayName("Should return the last three nibbles of the opcode")
+    void nnn() {
+        int expectedNNN = (nibble2 << 8) + (nibble3 << 4) + nibble4;
+
+        assertEquals(expectedNNN, opcode.nnn());
+    }
+
+    @Test
+    @DisplayName("Should return the integer value of the opcode")
+    void intValue() {
+        int expectedIntValue = (nibble1 << 12) + (nibble2 << 8) + (nibble3 << 4) + nibble4;
+
+        assertEquals(expectedIntValue, opcode.intValue());
+    }
+
+    @Test
+    @DisplayName("Should return true if two opcodes have the same integer value")
+    void testEquals_returnTrue() {
+        int firstByte = 0x0055;
+        int secondByte = 0x0022;
+        Opcode opcode1 = new Opcode(firstByte, secondByte);
+        Opcode opcode2 = new Opcode(firstByte, secondByte);
+
+        assertEquals(opcode1, opcode2);
+
+
+        int instruction = 0x13FF;
+        opcode1 = new Opcode(instruction);
+        opcode2 = new Opcode(instruction);
+
+        assertEquals(opcode1, opcode2);
+    }
+
+    @Test
+    @DisplayName("Should return false if two opcodes have the different integer values")
+    void testEquals_returnFalse() {
+        int firstByte = 0x0055;
+        int secondByte = 0x0022;
+        int differentSecondByte = 0x0044;
+        Opcode opcode1 = new Opcode(firstByte, secondByte);
+        Opcode opcode2 = new Opcode(firstByte, differentSecondByte);
+
+        assertNotEquals(opcode1, opcode2);
+
+
+        int instruction = 0x13FF;
+        int differentInstruction = 0x30AC;
+        opcode1 = new Opcode(instruction);
+        opcode2 = new Opcode(differentInstruction);
+        assertNotEquals(opcode1, opcode2);
+    }
+}
