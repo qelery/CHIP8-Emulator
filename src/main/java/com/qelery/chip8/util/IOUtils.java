53e952ccf82670d11eac0720424ebc47c46793b8
Quincy Elery <quincyelery@gmail.com>
2022-11-03 19:31:21 -0500

    Add private constructor to IOUtils class

--- src/main/java/com/qelery/chip8/util/IOUtils.java
+++ src/main/java/com/qelery/chip8/util/IOUtils.java
@@ -2,6 +2,10 @@ package com.qelery.chip8.util;
 
 public class IOUtils {
 
+    private IOUtils() {
+        throw new IllegalArgumentException("Utility class");
+    }
+
     public static void clearConsole() {
         System.out.println(new String(new char[50]).replace("\0", System.lineSeparator()));
     }
